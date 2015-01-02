64a9a3b708a13f3e9b4e48860fb5f517c92fdfa5
Nathan Palmer <nathan@sprucemedia.com>
2015-01-02 14:24:04 -0500

    Initial commit

---
+++ lib/mina/git/revision.rb
@@ -0,0 +1,9 @@
+require "mina/git/revision/version"
+
+module Mina
+  module Git
+    module Revision
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/mina/git/revision/version.rb
@@ -0,0 +1,7 @@
+module Mina
+  module Git
+    module Revision
+      VERSION = "0.0.1"
+    end
+  end
+end
